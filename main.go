ab16666940643281fc7d0084d85c19d19ebb9af7
Anton Petrov <anton.a.petrov@gmail.com>
2022-05-03 10:42:46 +0300

    Add gzip compresion

--- main.go
+++ main.go
@@ -18,12 +18,14 @@ func main() {
 	// Configure flags
 	compress := ""
 	decompress := ""
+	alg := ""
 	output := ""
 	test := 0
 	quality := 6
 	flag.StringVar(&compress, "c", "", "compress a file")
 	flag.StringVar(&decompress, "d", "", "decompress a file")
 	flag.StringVar(&output, "o", "", "output file")
+	flag.StringVar(&alg, "a", "gzip", "algorithm (brotli, gzip")
 	flag.IntVar(&test, "t", 0, "buffer size for benchmark test (megabytes)")
 	flag.IntVar(&quality, "q", 6, "compression quality (BestSpeed - 0, DefaultCompression - 6, BestCompression - 11)")
 	flag.Parse()
@@ -36,18 +38,26 @@ func main() {
 		input = compress
 		input_data = ReadFile(input)
 		println("Compressing data...")
-		output_data, _ = Compress(input_data, quality)
-		ioutil.WriteFile(input+".brotli", output_data, 0777)
+		if alg == "brotli" {
+			output_data, _ = Compress(input_data, quality)
+		}	else {
+			output_data, _ = CompressGzip(input_data)
+		}
+		ioutil.WriteFile(input+"."+alg, output_data, 0777)
 	} else if decompress != "" {
 		input = decompress
 		input_data = ReadFile(input)
 		println("Desompressing data...")
-		output_data, _ = Decompress(input_data)
+		if alg == "brotli" {
+			output_data, _ = Decompress(input_data)
+		} else {
+			output_data, _ = DecompressGzip(input_data)
+		}
 		ioutil.WriteFile(strings.TrimSuffix(input, filepath.Ext(input)), output_data, 0777)
 	} else if test != 0 {
 		println("Preparing benchmark...")
 		input_data = gen_rand_text(test * 100000)
-		println("Compressing data") // bytes.NewBuffer(input_data).String()
+		println("Compressing data")
 		output_data, _ = Compress(input_data, quality)
 		println("Desompressing data")
 		Decompress(output_data)
--- utils.go
+++ utils.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"compress/gzip"
+	"io"
 	"io/ioutil"
 	"log"
 	"math/rand"
@@ -31,7 +33,7 @@ func gen_rand_text(size int) []byte {
 
 // Compress returns content encoded with Brotli.
 func Compress(content []byte, quality int) ([]byte, error) {
-	defer timeTrack(time.Now(), "Compress")
+	defer timeTrack(time.Now(), "Compress Brotli")
 	var buf bytes.Buffer
 	writer := NewWriterOptions(&buf, WriterOptions{Quality: quality})
 	_, err := writer.Write(content)
@@ -47,7 +49,7 @@ func SimpleBenchmark(size_bytes int) {
 
 // Decompressing Brotli encoded data.
 func Decompress(encodedData []byte) ([]byte, error) {
-	defer timeTrack(time.Now(), "Deompress")
+	defer timeTrack(time.Now(), "Decompress Brotli")
 	r := NewReader(bytes.NewReader(encodedData))
 	return ioutil.ReadAll(r)
 }
@@ -59,4 +61,53 @@ func ReadFile(file string) []byte {
 		log.Fatal(err)
 	}
 	return data
+}
+
+
+func CompressGzip(data []byte) ([]byte, error) {
+	defer timeTrack(time.Now(), "Compress Gzip")
+	var compressed_data bytes.Buffer
+	zw := gzip.NewWriter(&compressed_data)
+
+	// Setting the Header fields is optional.
+	// zw.Name = ""
+	// zw.Comment = ""
+	// zw.ModTime = time.Date(1977, time.May, 25, 0, 0, 0, 0, time.UTC)
+
+	_, err := zw.Write(data)
+
+	if err != nil {
+		log.Println(err)
+		return data, err
+	}
+	if err := zw.Close(); err != nil {
+		log.Println(err)
+		return data, err
+	}
+
+	return compressed_data.Bytes(), err
+}
+
+func DecompressGzip(data []byte) ([]byte, error) {
+	defer timeTrack(time.Now(), "Decompress Gzip")
+	var compressed_data = bytes.NewBuffer(data)
+	var decompressed_data bytes.Buffer
+
+	zr, err:= gzip.NewReader(compressed_data)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	
+	if _, err := io.Copy(&decompressed_data, zr); err != nil {
+		log.Println(err)
+		return data, err
+	}
+
+	if err := zr.Close(); err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return decompressed_data.Bytes(), err
 }
